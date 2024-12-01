304fa27ee3dec49940863c2b2261ce0560ceb02c
Yousef Hatem <yousef26hatem@gmail.com>
2024-12-01 19:34:09 +0200

    Start of app

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1 align="center">Craftoria Server</h1>
+<p align="center">Craftoria node js server</p>
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const mountRoutes = (app) => {
+  // app.use("/api/v1/users");
+  // app.use("/api/v1/products");
+};
+
+module.exports = mountRoutes;
