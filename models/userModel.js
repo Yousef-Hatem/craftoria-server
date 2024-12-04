860f16c6c596fd195a09515c1752328580b379da
Yousef Hatem <yousef26hatem@gmail.com>
2024-12-04 22:47:07 +0200

    Refactor user mode and add verified and verifiedCode

--- models/userModel.js
+++ models/userModel.js
@@ -1,61 +1,76 @@
-const mongoose = require ('mongoose');
-const bcrypt = require('bcrypt');
-let userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, 'Name is Required'],
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Required"],
     },
-    email:{
-        type:String,
-        required:[true,'Email is Required'],
-        unique:true,
-        match:/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ 
+    email: {
+      type: String,
+      required: [true, "Email is Required"],
+      unique: true,
+      match: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
     },
-    password:{
-        type:String,
-        required:[true, 'Password is Required'],
+    verified: {
+      type: Boolean,
+      default: false,
     },
-    cart:{
-        items:[{
-            productId:{
-                type:mongoose.Schema.Types.ObjectId,
-                ref:'Product'
-            },
-            quantity:Number
-        }]
+    verifiedCode: String,
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
     },
-    role:{
-        type:String,
-        enum:['user','admin'],
-        default:'user'
+    passwordChangedAt: Date,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
-    address:[{
-        street:String,
-        buildingNumber:String,
-        city:String,
-        governorate:String,
-        zipCode:String,
-    }],
-    phone:[{
-        type:string,
-        match:/^01[0-5][0-9]{8}$/
-    }],
-    favorite:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Product'
-    }]
+    cart: {
+      items: [
+        {
+          productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+          },
+          quantity: Number,
+        },
+      ],
+    },
+    address: [
+      {
+        street: String,
+        buildingNumber: String,
+        city: String,
+        governorate: String,
+        zipCode: String,
+      },
+    ],
+    phone: [
+      {
+        type: String,
+        match: /^01[0-5][0-9]{8}$/,
+      },
+    ],
+    favorite: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-userSchema.pre('save',async function(next){
-    const user =this;
-    if(!user.isModified('password'))return next();
+const User = mongoose.model("User", userSchema);
 
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password,salt);
-    next();
-});
-
-
-
-const userModel = mongoose.model("User", userSchema);
-module.exports = userModel;
+module.exports = User;
