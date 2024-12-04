bbc53f52823a0bb9ae81783b0e4aa286538d080f
Yousef Hatem <yousef26hatem@gmail.com>
2024-12-04 22:28:02 +0200

    Fix send email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -18,7 +18,7 @@ const sendEmail = async (options) => {
     from: `Craftoria <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
-    text: options.message,
+    html: options.message,
   };
 
   await transporter.sendMail(mailOpts);
