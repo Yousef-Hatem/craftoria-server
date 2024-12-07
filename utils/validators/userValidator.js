6b1500ddf3777c711926872d0cea8f3e19aeb497
A'laa Mamdouh <alaamamdouh62@gmail.com>
2024-12-08 01:27:10 +0200

    fix with express-validator

--- utils/validators/userValidator.js
+++ utils/validators/userValidator.js
@@ -1,93 +1,122 @@
+const express = require('express');
 const mongoose = require('mongoose');
-const {isEmail, isStrongPassword} = requier('validator');
-const User = require('/models/userModel')
+const bcrypt = require('bcrypt');
+const { check , body} = require("express-validator");
+const User = require("../../models/userModel");
+
+exports.createUser = [
+    check('name')
+        .notEmpty()
+        .isLength({min:3})
+        .withMessage('Name must be at least 3 characters long.'),
+    check('email')
+        .notEmpty()
+        .withMessage("Email required")
+        .isEmail()
+        .withMessage("Invalid email address"),
+    check('password')
+        .notEmpty()
+        .withMessage("Password required")
+        .isLength({ min: 6 })
+        .isStrongPassword()
+        .withMessage('Password must be at least 6 characters long.')
+]
+
+exports.getUser = [
+    check('userId')
+        .isMongoId()
+        .withMessage('Invalid user ID')
+        .custom(async (value)=>{
+            const user = await User.findById(value);
+            if (!user){
+                throw new Error('User not found');
+            }
+            return true;
+        })
+]
+
+exports.updateUser = [
+    check('userId')
+        .isMongoId()
+        .withMessage('Invalid user ID')
+        .custom(async (value)=>{
+            const user = await User.findById(value);
+            if (!user){
+                throw new Error('User not found');
+            }
+            return true;
+        }),
+        body('name')
+            .optional()
+            .isString()
+            .trim(),
+        body('email')
+            .optional()
+            .isEmail()
+            .normaliseEmail()
+]
+
+exports.deleteUser = [
+    check('userId')
+        .isMongoId()
+        .withMessage('Invalid user ID')
+        .custom(async (value)=>{
+            const user = await User.findById(value);
+            if (!user){
+                throw new Error('User not found');
+            }
+            return true;
+        }),
+        body('password')
+            .optional()
+            .custom(async (value,{req})=>{
+                const user = req.user;
+                const isMatch = await bcrypt.compare(value,user.password);
+                if (!isMatch){
+                    throw new Error('Incorrect password');
+                }
+                return true;
+            })
+]
 
 
-//Create User Validation
-function createUser(userData){
-    const errors = [];
+exports.updateLoggedUserData = [
+    check('userId')
+        .isMongoId()
+        .withMessage('Invalid user ID')
+        .custom(async (value)=>{
+            const user = await User.findById(value);
+            if (!user){
+                throw new Error('User not found');
+            }
+            return true;
+        }),
+        body('password')
+            .custom(async (value,{req})=>{
+                const user = req.user;
+                const isMatch = await bcrypt.compare(value,user.password);
+                if (!isMatch){
+                    throw new Error('Incorrect password');
+                }
+                return true;
+            }),
+        body('newName')
+            .optional()
+            .isString()
+            .trim(),
+        body('newEmail')
+            .optional()
+            .isEmail()
+            .normaliseEmail(),
+        body('newPassword')
+            .optional()
+            .isStrongPassword()
+            .custom(async (value, {req})=>{
+                if(value){
+                    const hashedPassword = await bcrypt.hash(value,10);
+                    req.newPassword = hashedPassword;
+                }
+                return true;
+            })
 
-    if (userData.name.length <3 || userData.name.length > 30){
-        errors.push('Name must be between 3 and 30 characters');
-    }
-
-    if (!isEmail(userData.email)){
-        errors.push('Email must be a valid email address');
-    }
-
-    if (isStrongPassword(userData.password,{
-        minLength: 6,
-        minLowercase:1,
-        minUppercase:1,
-        minNumber:1,
-        minSymbols:1
-    })){
-        errors.push('Password must be at least 6 characters long');
-    }
-
-    if (userData.phoneNumber && !/^\+20\d{10}$/.test(userData.phoneNumber) && !/^\d{11}$/.test(userData.phoneNumber) ){
-        errors.push('Invalid phone number');
-    }
-    
-    return errors;
-}
-
-function isValidObjectId(id){
-    return mongoose.Types.ObjectId.isValid(id);
-}
-
-//Get User Validation
-async function getUser(userId){
-    if (!isValidObjectId(userId)){
-        throw new Error('Invalid user id');
-    }
-
-    const user = await User.findById(userId);
-    if (!user){
-        throw new Error('User not found');
-    }
-}
-
-//Update User Validation
-async function updateUser(userId, updatedData){
-    if (!isValidObjectId(userId)){
-        throw new Error('Invalid user ID');
-    }
-
-    const user = await User.findById(userId);
-    if(!user){
-        throw new Error('User not found');
-    }
-
-    Object.assign(user, updatedData);
-    await user.save();
-
-    return user;
-}
-
-//Delete User Validation
-async function deleteUser(userId){
-    if(!isValidObjectId(userId)){
-        throw new Error('Invalid user ID');
-    }
-
-    const user = await User.findByIdAndDelete(userId);
-    if(!user){
-        throw new Error('User not found');
-    }
-}
-
-async function updateLoggedUserData(userId, updatedData){
-    if(!isValidObjectId(userId)){
-        throw new Error('Invalid user ID');
-    }
-
-    const user = await User.findByIdAndUpdate(userId, updatedData, {new: true});
-    if(!user){
-        throw new Error('User not found');
-    }
-
-    return user;
-}
-
-module.exports ={createUser, getUser, updateUser, deleteUser, updateLoggedUserData}
+]
