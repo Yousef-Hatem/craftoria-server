07f443e09f0a5eb8eed6ed1e4f74ea24c723405f
Yousef Hatem <yousef26hatem@gmail.com>
2025-01-05 20:11:32 +0200

    Cart validator

--- utils/validators/cartValidator.js
+++ utils/validators/cartValidator.js
@@ -1,14 +1,60 @@
 const { check } = require("express-validator");
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const Product = require("../../models/productModel");
 
-exports.validateCart = [
-    check('productId')
+exports.addItemToCartValidator = [
+  check("productId")
+    .notEmpty()
+    .withMessage("Product id is required")
     .isMongoId()
-    .withMessage('Product Id is invalid'),
-    check('quantity')
+    .withMessage("Product id is invalid")
+    .custom((id) =>
+      Product.findById(id).then((product) => {
+        if (!product) {
+          return Promise.reject(new Error(`There is no product for this id`));
+        }
+      })
+    ),
+  validatorMiddleware,
+];
+
+exports.updateItemQuantityValidator = [
+  check("productId")
+    .isMongoId()
+    .withMessage("Product id is invalid")
+    .custom((id, { req }) => {
+      const itemIndex = req.user.cart.findIndex(
+        (item) => item.productId.toString() === id
+      );
+      if (itemIndex === -1) {
+        throw new Error(`There is no product for this id`);
+      }
+      req.body.itemIndex = itemIndex;
+      return true;
+    }),
+  check("quantity")
+    .notEmpty()
+    .withMessage("Quantity required")
     .isNumeric()
-    .withMessage('Quantity must be a number.')
-    .isInt({min: 1})
-    .withMessage('Quantity must be an integer bigger than zero.'),
-    validatorMiddleware,
+    .withMessage("Quantity must be a number")
+    .isInt({ min: 1 })
+    .withMessage("Quantity must be an integer bigger than zero."),
+  validatorMiddleware,
+];
+
+exports.removeItemValidator = [
+  check("productId")
+    .isMongoId()
+    .withMessage("Product id is invalid")
+    .custom((id, { req }) => {
+      const itemIndex = req.user.cart.findIndex(
+        (item) => item.productId.toString() === id
+      );
+      if (itemIndex === -1) {
+        throw new Error(`There is no product for this id`);
+      }
+      req.body.itemIndex = itemIndex;
+      return true;
+    }),
+  validatorMiddleware,
 ];
