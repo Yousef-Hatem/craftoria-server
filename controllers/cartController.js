4b3c9975828eae9dc4f4c57419f008d1d1c73873
Yousef Hatem <yousef26hatem@gmail.com>
2025-01-05 20:10:59 +0200

    Optimize cartController logic and structure

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,45 +1,42 @@
 const asyncHandler = require("express-async-handler");
 
-const User = require("../models/userModel");
-const Product = require("../models/productModel");
-
 exports.addItemToCart = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.productId);
-    const existingProduct = req.user.cart.find(item => item.productId.toString() === product._id.toString());
+  const { productId } = req.params;
 
-    if (existingProduct) {
-        existingProduct.quantity += 1;
-    } else {
-        req.user.cart.push({ productId: product._id, quantity: 1 });
-    }
-    await req.user.save();
-    res.json(req.user.cart);
+  const existingProductIndex = req.user.cart.findIndex(
+    (item) => item.productId.toString() === productId
+  );
+
+  if (existingProductIndex === -1) {
+    req.user.cart.push({ productId });
+  } else {
+    req.user.cart[existingProductIndex].quantity += 1;
+  }
+
+  await req.user.save();
+
+  res.json({ data: req.user.cart });
 });
 
-exports.updateItemsQuantity = asyncHandler(async (req, res) => {
-        const user = await User.findById(req.user._id);
-        const product = req.params.productId;
-        const newQuantity = req.body.quantity;
-
-        const cartItem = user.cart.find(item => item.productId.toString() === product)
-        if (cartItem) {
-            cartItem.quantity = newQuantity;
-            await user.save();
-            res.json(user.cart);
-        } else {
-            res.status(404).json({ message: 'Product not found' })
-    }
+exports.getLoggedUserCart = asyncHandler(async (req, res) => {
+  res.json({ data: req.user.cart });
 });
 
-exports.removeProduct = asyncHandler(async(req,res) =>{
-    const user = await User.findById(req.user._id);
-    const product = req.params.productId;
-    user.cart = user.cart.filter(item =>item.productId.toString() !== product);
-    
-    if(user.isModified('cart')){
-        await user.save();
-        res.json({message:'Product deleted successfully'});
-    }else{
-        res.status(404).json({message: 'Product not found in cart'});
-    }
+exports.updateItemQuantity = asyncHandler(async (req, res) => {
+  const { itemIndex, quantity } = req.body;
+
+  req.user.cart[itemIndex].quantity = quantity;
+  await req.user.save();
+
+  res.json({ data: req.user.cart });
+});
+
+exports.removeItem = asyncHandler(async (req, res) => {
+  const { itemIndex } = req.body;
+
+  req.user.cart.splice(itemIndex, 1);
+
+  await req.user.save();
+
+  res.json({ data: req.user.cart });
 });
