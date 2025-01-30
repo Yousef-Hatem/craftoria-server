25545b72e44e102cf4f82e65205cc6c26d719196
Olaa Qoutb <olaaahmed750@gmail.com>
2025-01-30 10:44:31 +0200

    Added address and favorite controllers and routes

---
+++ controllers/favoriteController.js
@@ -0,0 +1,45 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const ApiError = require("../utils/apiError");
+
+// Add product to favorites
+
+exports.addfavorite = asyncHandler(async (req, res, next) => {
+  const { productId } = req.body;
+
+
+  const user = await User.findById(req.user._id);
+  
+  user.favorite.push(productId);
+  await user.save();
+
+  res
+    .status(200)
+    .json({ message: "Product added to favorites", data: user.favorite });
+});
+
+// Remove product from favorites
+
+exports.removeFavorite = asyncHandler(async (req, res, next) => {
+  const { productId } = req.body;
+
+ 
+
+  const user = await User.findById(req.user._id);
+
+ 
+
+  user.favorite = user.favorite.filter((id) => id.toString() !== productId);
+  await user.save();
+  res
+    .status(200)
+    .json({ message: "Product removed from favorites", data: user.favorite });
+});
+
+// Get all favorites for the logged-in user
+
+exports.getfavorites = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id).populate("favorite");
+
+  res.status(200).json({ data: user.favorite });
+});
