0beb4ecc1a398afb9fcb48be14200708558d9254
Olaa Qoutb <olaaahmed750@gmail.com>
2025-01-30 10:58:20 +0200

    Update Address  last version

--- routes/addressRoute.js
+++ routes/addressRoute.js
@@ -1,29 +1,22 @@
+// routes\addressRoute.js
+// routes\addressRoute.js
 const express = require("express");
 const {
-    addAddress,
-    updateAddress,
-    deleteAddress,
-    getAddresses,
-    getAddressById
-}=require("../controllers/addressController")
-const authService =require("../controllers/authController");
+  addAddress,
+  updateAddress,
+  deleteAddress,
+  getAddresses,
+  getAddressById,
+} = require("../controllers/addressController");
+const authService = require("../controllers/authController");
 
 const router = express.Router();
-router.use(authService.protect); 
-router.get("/",getAddresses)
-router.post("/",addAddress)
-router.put("/:addressId",updateAddress)
-router.delete("/:addressId",deleteAddress)
+router.use(authService.protect);
+router.get("/", getAddresses);
+router.post("/", addAddress);
+router.put("/:addressId", updateAddress);
+router.delete("/:addressId", deleteAddress);
 
-
-router.get("/:id",getAddressById);
-
-
-
-
-
+router.get("/:id", getAddressById);
 
 module.exports = router;
-
-
-
