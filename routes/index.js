2a6382894290289dc35e58f8342a6e429444322e
Olaa Qoutb <olaaahmed750@gmail.com>
2025-01-30 10:24:08 +0200

    product controller and route and index last version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const authRoute = require("./authRoute");
 const userRoute = require("./userRoute");
+const productRoute = require("./productRoute");
+const addressRoute = require("./addressRoute");
+const favoriteRoute = require("./favoriteRoute")
 
 const mountRoutes = (app) => {
   app.use("/api/v1/auth", authRoute);
   app.use("/api/v1/users", userRoute);
+  app.use("/api/v1/products", productRoute);
+  app.use("/api/v1/address", addressRoute);
+  app.use("/api/v1/favorites", favoriteRoute);
+
 };
 
 module.exports = mountRoutes;
