50ec3dbe498971b6a02830d6bdafa6a3d40f7399
Eng/mohamed salah <68165476+mosalah222@users.noreply.github.com>
2025-02-01 22:58:47 +0200

    edited

---
+++ utils/validators/couponValidator.js
@@ -0,0 +1,20 @@
+const {body, validationResult } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+
+
+const couponValidator = [
+  body("code").isString().notEmpty().withMessage("Coupon code is required"),
+  body("discount").isNumeric().withMessage("Discount must be a number"),
+  body("expirationDate").isISO8601().toDate().withMessage("Expiration date must be a valid date"),
+  validatorMiddleware
+];
+
+const validateCoupon = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ success: false, errors: errors.array() });
+  }
+  next();
+};
+
+module.exports = { couponValidator, validateCoupon };
