aee7bb30788e8e44cb40d3a83e2a14675e7a9706
Eng/mohamed salah <68165476+mosalah222@users.noreply.github.com>
2025-02-06 04:12:27 +0200

    updated

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -5,11 +5,15 @@ const {
   getCouponById,
   updateCoupon,
   deleteCoupon,
-} = require("./controllers/couponController");
+} = require("../controllers/CouponController");
 const { couponValidator, validateCoupon } = require("../middlewares/validatorMiddleware");
 
 const router = express.Router();
 
-router.post("/", couponValidator, validateCoupon, createCoupon);
-router.get("/", getCoupons);
+router.post("/createCopoun", couponValidator, validateCoupon, createCoupon);
+router.get("/getCopouns", getCoupons);
 router.get("/:id", getCouponById);
+router.put('/:id', couponValidation, updateCoupon);
+router.delete('/:id', deleteCoupon);
+
+module.exports = router;
--- utils/validators/couponValidator.js
+++ utils/validators/couponValidator.js
@@ -5,16 +5,17 @@ const validatorMiddleware = require("../../middlewares/validatorMiddleware");
 const couponValidator = [
   body("code").isString().notEmpty().withMessage("Coupon code is required"),
   body("discount").isNumeric().withMessage("Discount must be a number"),
+  body("percentageDiscount").isNumeric().withMessage("percentageDiscount must be a number"),
   body("expirationDate").isISO8601().toDate().withMessage("Expiration date must be a valid date"),
   validatorMiddleware
 ];
 
-const validateCoupon = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ success: false, errors: errors.array() });
-  }
-  next();
-};
+// const validateCoupon = (req, res, next) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return res.status(400).json({ success: false, errors: errors.array() });
+//   }
+//   next();
+// };
 
 module.exports = { couponValidator, validateCoupon };
