0d6e05ed046d7250a199bb33f7d7fca3c11cd99f
Eng/mohamed salah <68165476+mosalah222@users.noreply.github.com>
2025-02-06 04:19:09 +0200

    updated

--- utils/validators/couponValidator.js
+++ utils/validators/couponValidator.js
@@ -10,6 +10,64 @@ const couponValidator = [
   validatorMiddleware
 ];
 
+const createCouponValidator = [
+  body("code")
+    .isString()
+    .notEmpty()
+    .withMessage("Coupon code is required"),
+  body("discount")
+    .isNumeric()
+    .withMessage("Discount must be a number"),
+  body("percentageDiscount")
+    .optional()
+    .isNumeric()
+    .withMessage("percentageDiscount must be a number"),
+  body("expirationDate")
+    .isISO8601()
+    .toDate()
+    .withMessage("Expiration date must be a valid date"),
+  validatorMiddleware
+];
+
+const getCouponByIdValidator = [
+  param("id")
+    .isMongoId()
+    .withMessage("Invalid coupon ID format"),
+  validatorMiddleware
+];
+
+
+const updateCouponValidator = [
+  param("id")
+    .isMongoId()
+    .withMessage("Invalid coupon ID format"),
+  body("code")
+    .optional() // This field is optional
+    .isString()
+    .notEmpty()
+    .withMessage("Coupon code must be a non-empty string"),
+  body("discount")
+    .optional() // This field is optional
+    .isNumeric()
+    .withMessage("Discount must be a number"),
+  body("percentageDiscount")
+    .optional() // This field is optional
+    .isNumeric()
+    .withMessage("percentageDiscount must be a number"),
+  body("expirationDate")
+    .optional() // This field is optional
+    .isISO8601()
+    .toDate()
+    .withMessage("Expiration date must be a valid date"),
+  validatorMiddleware
+];
+
+const deleteCouponValidator = [
+  param("id")
+    .isMongoId()
+    .withMessage("Invalid coupon ID format"),
+  validatorMiddleware
+];
 // const validateCoupon = (req, res, next) => {
 //   const errors = validationResult(req);
 //   if (!errors.isEmpty()) {
@@ -18,4 +76,4 @@ const couponValidator = [
 //   next();
 // };
 
-module.exports = { couponValidator, validateCoupon };
+module.exports = { couponValidator, validateCoupon, createCouponValidator,getCouponByIdValidator,updateCouponValidator,deleteCouponValidator };
