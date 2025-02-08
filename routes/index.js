bae566e4b66f03012678b64ff06bb2f161f52468
Yousef Hatem <yousef26hatem@gmail.com>
2025-02-08 14:41:09 +0200

    Category route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const authRoute = require("./authRoute");
 const userRoute = require("./userRoute");
 const cartRoute = require("./cartRoute");
+const categoryRoute = require("./categoryRoute");
 
 const mountRoutes = (app) => {
   app.use("/api/v1/auth", authRoute);
   app.use("/api/v1/users", userRoute);
   app.use("/api/v1/cart", cartRoute);
+  app.use("/api/v1/categories", categoryRoute);
 };
 
 module.exports = mountRoutes;
