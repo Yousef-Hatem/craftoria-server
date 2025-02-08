c0859d2b088a26bd7952a63c02652cb1703321c0
Yousef Hatem <yousef26hatem@gmail.com>
2025-02-08 14:40:51 +0200

    Category Controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,133 +1,28 @@
-const Category = require('../models/categoryModel');
+const Factory = require("./handlersFactory");
+const Category = require("../models/categoryModel");
+const { sanitizeCategory } = require("../utils/sanitize/sanitizeCategory");
+const {
+  uploadSingleImage,
+  resizeImage,
+} = require("../middlewares/uploadImageMiddleware");
 
+const factory = new Factory(Category);
 
-exports.createCategory = async (req, res) => {
-    try {
-        const { name, description, image } = req.body;
+exports.uploadImage = uploadSingleImage("image");
 
-        
-        const category = new Category({
-            name,
-            description,
-            image
-        });
+exports.resizeImage = resizeImage({
+  model: Category,
+  fieldName: "image",
+  resize: [500, 500],
+  allowSavingInPngFormat: true,
+});
 
+exports.getCategories = factory.getAll();
 
-        await category.save();
+exports.getCategory = factory.getOne();
 
-        res.status(201).json({
-            success: true,
-            message: 'Category created successfully',
-            data: category
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: 'Failed to create category',
-            error: error.message
-        });
-    }
-};
+exports.createCategory = factory.createOne(sanitizeCategory);
 
+exports.updateCategory = factory.updateOne({ imageKey: "image" });
 
-exports.getAllCategories = async (req, res) => {
-    try {
-        const categories = await Category.find();
-        res.status(200).json({
-            success: true,
-            data: categories
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: 'Failed to fetch categories',
-            error: error.message
-        });
-    }
-};
-
-
-exports.getCategoryById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const category = await Category.findById(id);
-
-        if (!category) {
-            return res.status(404).json({
-                success: false,
-                message: 'Category not found'
-            });
-        }
-
-        res.status(200).json({
-            success: true,
-            data: category
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: 'Failed to fetch category',
-            error: error.message
-        });
-    }
-};
-
-
-exports.updateCategory = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { name, description, image } = req.body;
-
-        const category = await Category.findByIdAndUpdate(
-            id,
-            { name, description, image },
-            { new: true, runValidators: true } 
-        );
-
-        if (!category) {
-            return res.status(404).json({
-                success: false,
-                message: 'Category not found'
-            });
-        }
-
-        res.status(200).json({
-            success: true,
-            message: 'Category updated successfully',
-            data: category
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: 'Failed to update category',
-            error: error.message
-        });
-    }
-};
-
-
-exports.deleteCategory = async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const category = await Category.findByIdAndDelete(id);
-
-        if (!category) {
-            return res.status(404).json({
-                success: false,
-                message: 'Category not found'
-            });
-        }
-
-        res.status(200).json({
-            success: true,
-            message: 'Category deleted successfully'
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: 'Failed to delete category',
-            error: error.message
-        });
-    }
-};
+exports.deleteCategory = factory.deleteOne();
