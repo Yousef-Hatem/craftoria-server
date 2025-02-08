278b57898ade7afd29f07346305016ac9d4f1265
Yousef Hatem <yousef26hatem@gmail.com>
2025-02-08 19:36:26 +0200

    Update Product Controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,80 +1,31 @@
-const productModel=require("../models/productModel")
+const Factory = require("./handlersFactory");
+const Product = require("../models/productModel");
+const {
+  uploadMixOfImages,
+  resizeImages,
+} = require("../middlewares/uploadImageMiddleware");
 
-let getProduct= async(req,res)=>{
-try{
-const products=await productModel.find().populate("id");
-res.status(200).json({message:"Product fetched successfully",data:products})
-}
-catch(err){
-res.status(400).json({message:err.message})
-}
-};
+const factory = new Factory(Product, { populate: "category" });
 
+exports.uploadImages = uploadMixOfImages([
+  {
+    name: "images",
+    maxCount: 10,
+  },
+]);
 
-let postProduct=async(req,res)=>{
-    const newProduct=req.body
-    try{
-const createProduct=await productModel.create(newProduct)
+exports.resizeImages = resizeImages({
+  model: Product,
+  fieldName: "images",
+  resize: [800, 800],
+});
 
-res.status(200).json({message:"The product created successfully",date:createProduct})
-}
-    catch(err){
-        res.status(400).json({message:err.message})
-    }
-}
+exports.getProducts = factory.getAll();
 
+exports.getProduct = factory.getOne();
 
-let getProductById=async(req,res)=>{
-    const {id}=req.params
-    try{
-        const getProduct=await productModel.findById(id)
+exports.createProduct = factory.createOne();
 
-        if(getProduct){
-            res.status(200).json({message:"Product found successfully",data:getProduct})
-        }else{
-            res.status(404).json({message:" Product not found"})
-        }
-    }
-    catch(err){
-        res.status(400).json({message:err.message})
-    }
-}
+exports.updateProduct = factory.updateOne();
 
-
-
-let deleteSpecificProduct= async(req,res)=>{
-    const {id}=req.params
-    try{
-        const deleteProduct=await productModel.findByIdAndDelete(id)
-        if( deleteProduct){
-            res.status(200).json({message:"Product deleted successfully",data:deleteProduct})
-        }else{
-            res.status(404).json({message:"Product not found"})
-        }
-    }catch(err){
-        res.status(400).json({message:err.message})
-    }
-}
-
-
-
-
-let updatedProduct=async(req,res)=>{
-    const {id}=req.params
-    const updates=req.body
-    try{
-        const update=await productModel.findByIdAndUpdate(id,updates,{
-            new:true,
-            runValidators:true
-        })
-        if(!update){
-            res.status(404).json({message:"Product not found"})
-        }
-        res.status(200).json({message:"Product updated",data:update})
-    }
-    catch(err){
-        res.status(400).json({message:err.message})
-    }
-};
-
-module.exports ={getProduct,postProduct,getProductById,deleteSpecificProduct,updatedProduct}
+exports.deleteProduct = factory.deleteOne();
